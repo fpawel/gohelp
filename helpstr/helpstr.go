2dce1c2fee8e0b7ac621834e551f52e65af7da9c
binf1611@gmail.com <fpawel>
2019-07-11 01:11:46 +0300

    FormatMerryStacktrace

--- helpstr/helpstr.go
+++ helpstr/helpstr.go
@@ -1,7 +1,11 @@
 package helpstr
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/ansel1/merry"
+	"path/filepath"
+	"runtime"
 	"time"
 )
 
@@ -18,4 +22,29 @@ func FormatDuration(d time.Duration) string {
 	s := d / time.Second
 
 	return fmt.Sprintf("%02d:%02d:%02d", h, m, s)
+}
+
+// FormatMerryStacktrace returns the error's stacktrace as a string formatted
+// the same way as golangs runtime package.
+// If e has no stacktrace, returns an empty string.
+func FormatMerryStacktrace(e error) string {
+	s := merry.Stack(e)
+	if len(s) == 0 {
+		return ""
+	}
+	buf := bytes.Buffer{}
+	for i, fp := range s {
+		fnc := runtime.FuncForPC(fp)
+		if fnc != nil {
+			f, l := fnc.FileLine(fp)
+			name := filepath.Base(fnc.Name())
+			ident := " "
+			if i > 0 {
+				ident = "\t"
+			}
+			buf.WriteString(fmt.Sprintf("%s%s:%d %s\n", ident, f, l, name))
+		}
+	}
+	return buf.String()
+
 }
