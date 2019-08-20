25483534d0119db82b0f32bdca805274564db1bc
binf1611@gmail.com <fpawel>
2019-08-20 10:06:16 +0300

    LogAppendPrefixKeys

--- structlog.go
+++ structlog.go
@@ -4,6 +4,20 @@ import (
 	"github.com/powerman/structlog"
 )
 
+func LogAppendPrefixKeys(log *structlog.Logger, args ...interface{}) *structlog.Logger {
+	var keys []string
+	for i, arg := range args {
+		if i%2 == 0 {
+			k, ok := arg.(string)
+			if !ok {
+				panic("key must be string")
+			}
+			keys = append(keys, k)
+		}
+	}
+	return log.New(args...).AppendPrefixKeys(keys...)
+}
+
 func LogPrependSuffixKeys(log *structlog.Logger, args ...interface{}) *structlog.Logger {
 	var keys []string
 	for i, arg := range args {
@@ -15,7 +29,6 @@ func LogPrependSuffixKeys(log *structlog.Logger, args ...interface{}) *structlog
 			keys = append(keys, k)
 		}
 	}
-
 	return log.New(args...).PrependSuffixKeys(keys...)
 }
 
