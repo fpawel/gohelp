9444fbafb88984e2de9e1c0a2a8f379a9865058b
binf1611@gmail.com <fpawel>
2019-09-13 14:36:09 +0300

    sqlite: Open and MustOpen

--- sqlite.go
+++ sqlite.go
@@ -6,17 +6,33 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func OpenSqliteDBx(fileName string) *sqlx.DB {
-	return sqlx.NewDb(OpenSqliteDB(fileName), "sqlite3")
+func MustOpenSqliteDBx(fileName string) *sqlx.DB {
+	return sqlx.NewDb(MustOpenSqliteDB(fileName), "sqlite3")
 }
 
-func OpenSqliteDB(fileName string) *sql.DB {
-	conn, err := sql.Open("sqlite3", fileName)
+func MustOpenSqliteDB(fileName string) *sql.DB {
+	conn, err := OpenSqliteDB(fileName)
 	if err != nil {
 		panic(err)
+	}
+	return conn
+}
+
+func OpenSqliteDBx(fileName string) (*sqlx.DB, error) {
+	c, err := OpenSqliteDB(fileName)
+	if err != nil {
+		return nil, err
+	}
+	return sqlx.NewDb(c, "sqlite3"), nil
+}
+
+func OpenSqliteDB(fileName string) (*sql.DB, error) {
+	conn, err := sql.Open("sqlite3", fileName)
+	if err != nil {
+		return nil, err
 	}
 	conn.SetMaxIdleConns(1)
 	conn.SetMaxOpenConns(1)
 	conn.SetConnMaxLifetime(0)
-	return conn
+	return conn, err
 }
